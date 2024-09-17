74013f304134fa532f7c60a725a3aadceda0ba03
SupRiyA-Osman <90715105+SupRiyA-Osman@users.noreply.github.com>
2024-09-17 09:27:35 +0600

    Add files via upload

---
+++ gyroscope.ino
@@ -0,0 +1,46 @@
+#include <Wire.h>
+
+#include <MPU6050.h>
+
+MPU6050 mpu;
+
+void setup() {
+
+Serial.begin(9600);
+Wire.begin();
+mpu.initialize();
+// Check if the connection was successful
+if (mpu.testConnection()) {
+Serial.println("MPU6050 connection successful");
+} else {
+
+Serial.println("MPU6050 connection failed");
+}
+
+}
+
+void loop() {
+
+int16_t ax, ay, az;
+
+int16_t gx, gy, gz;
+
+// Read accelerometer and gyroscope data
+
+mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+// Print the data
+
+Serial.print("aX = "); Serial.print(ax);
+
+Serial.print(" | aY = "); Serial.print(ay);
+
+Serial.print(" | aZ = "); Serial.print(az);
+
+Serial.print(" | gX = "); Serial.print(gx);
+
+Serial.print(" | gY = "); Serial.print(gy);
+
+Serial.print(" | gZ = "); Serial.println(gz);
+
+}
